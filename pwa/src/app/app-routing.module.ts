2d7e7ceed3e23c714a14ba28fe3e553fcc34e0d8
¨Trashe <trashe.racer@protonmail.com>
2020-08-24 00:07:22 +0100

    Layout navigation between home, country and language views

--- pwa/src/app/app-routing.module.ts
+++ pwa/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {LanguageComponent} from './language/language.component'
+import { CountryComponent } from './country/country.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'language', component: LanguageComponent },
+  { path: 'country', component: CountryComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' } // Redirect to `home`.
 ];
 
--- pwa/src/app/app.module.ts
+++ pwa/src/app/app.module.ts
@@ -5,11 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LanguageComponent } from './language/language.component';
+import { CountryComponent } from './country/country.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LanguageComponent,
+    CountryComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
