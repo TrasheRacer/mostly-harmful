ea6d5be842b5c15941fd1aae61636253e8643820
¨Trashe <trashe.racer@protonmail.com>
2020-08-25 15:08:58 +0100

    Create about view

--- pwa/src/app/app.module.ts
+++ pwa/src/app/app.module.ts
@@ -20,6 +20,9 @@ import { ForwardLanguageComponent } from './language/forward-language/forward-la
 import { StateSelectComponent } from './country/state-select/state-select.component';
 import { StateDisplayComponent } from './country/state-display/state-display.component';
 import { StateConfirmComponent } from './country/state-confirm/state-confirm.component';
+import { AboutComponent } from './about/about.component';
+import { BackComponent } from './about/back/back.component';
+import { ConfirmComponent } from './about/confirm/confirm.component';
 
 function createTranslateLoader(httpClient: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
@@ -40,7 +43,10 @@ function createTranslateLoader(httpClient: HttpClient): TranslateHttpLoader {
     ForwardLanguageComponent,
     StateSelectComponent,
     StateDisplayComponent,
-    StateConfirmComponent
+    StateConfirmComponent,
+    AboutComponent,
+    BackComponent,
+    ConfirmComponent
   ],
   imports: [
     BrowserModule,
