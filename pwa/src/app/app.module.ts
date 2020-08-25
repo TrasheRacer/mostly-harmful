ef4e0795eca49e081f6fbf8ed334fb1e94a41b7d
¨Trashe <trashe.racer@protonmail.com>
2020-08-25 17:57:34 +0100

    Remove child components from about view

--- pwa/src/app/app.module.ts
+++ pwa/src/app/app.module.ts
@@ -20,9 +20,6 @@ import { ForwardLanguageComponent } from './language/forward-language/forward-la
 import { StateSelectComponent } from './country/state-select/state-select.component';
 import { StateDisplayComponent } from './country/state-display/state-display.component';
 import { StateConfirmComponent } from './country/state-confirm/state-confirm.component';
-import { AboutComponent } from './about/about.component';
-import { BackComponent } from './about/back/back.component';
-import { ConfirmComponent } from './about/confirm/confirm.component';
 
 function createTranslateLoader(httpClient: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
@@ -44,9 +41,6 @@ function createTranslateLoader(httpClient: HttpClient): TranslateHttpLoader {
     StateSelectComponent,
     StateDisplayComponent,
     StateConfirmComponent,
-    AboutComponent,
-    BackComponent,
-    ConfirmComponent
   ],
   imports: [
     BrowserModule,
