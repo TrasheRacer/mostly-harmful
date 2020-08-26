befeef537470761c932db168ecd0119088670c80
¨Trashe <trashe.racer@protonmail.com>
2020-08-26 04:20:13 +0100

    Add binding to confirm component

--- pwa/src/app/view-template/confirm/confirm.component.ts
+++ pwa/src/app/view-template/confirm/confirm.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-confirm',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirm.component.css']
 })
 export class ConfirmComponent implements OnInit {
+
+  @Input() selection: string
 
   constructor() { }
 
