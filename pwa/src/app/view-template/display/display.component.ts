3082323d4f9f8cc8f94d3412249170ffc44f1d50
¨Trashe <trashe.racer@protonmail.com>
2020-08-26 05:54:31 +0100

    Add pre-translation to display component

--- pwa/src/app/view-template/display/display.component.ts
+++ pwa/src/app/view-template/display/display.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DisplayComponent implements OnInit {
 
+  @Input() name: string
   @Input() selection: string
 
   constructor() { }
