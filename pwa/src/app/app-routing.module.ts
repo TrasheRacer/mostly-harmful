71996c6b4761b9713992b033ee26e61d35fc9b61
¨Trashe <trashe.racer@protonmail.com>
2020-08-26 06:59:11 +0100

    Add ethnicity and gender views to identity flow

--- pwa/src/app/app-routing.module.ts
+++ pwa/src/app/app-routing.module.ts
@@ -5,10 +5,14 @@ import { SettingsComponent } from './settings/settings.component';
 import { StartComponent } from './start/start.component';
 import { IdentityComponent } from './identity/identity.component';
 import { AboutComponent } from './about/about.component';
+import { EthnicityComponent } from './identity/ethnicity/ethnicity.component';
+import { GenderComponent } from './identity/gender/gender.component';
 
 const routes: Routes = [
   { path: ':region', component: HomeComponent },
   { path: ':region/identity', component: IdentityComponent },
+  { path: ':region/identity/ethnicity', component: EthnicityComponent },
+  { path: ':region/identity/gender', component: GenderComponent },
   { path: ':region/help', component: StartComponent },
   { path: ':region/settings', component: SettingsComponent },
   { path: ':region/about', component: AboutComponent },
