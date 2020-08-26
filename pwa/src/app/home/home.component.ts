30b66a93d19e38c8620f1c7adb0bce2f31a2fea6
¨Trashe <trashe.racer@protonmail.com>
2020-08-26 03:46:48 +0100

    Pass input into display component

--- pwa/src/app/home/home.component.ts
+++ pwa/src/app/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NoSelection } from './../../globals'
+
+type FlowSelection = 'start' | 'about'
+
+type Selection = NoSelection | FlowSelection
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  selection: Selection
 
   constructor() { }
 
