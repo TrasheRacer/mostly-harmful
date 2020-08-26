48d1bf9456b31afc8d874c0d906168066d5b689a
¨Trashe <trashe.racer@protonmail.com>
2020-08-26 01:16:06 +0100

    Add about view created from view module

--- pwa/src/app/app-routing.module.ts
+++ pwa/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {LanguageComponent} from './language/language.component';
 import { CountryComponent } from './country/country.component';
+import { ViewComponent } from './view/view.component';
 
 const routes: Routes = [
   { path: 'home/:state', component: HomeComponent },
   { path: 'language/:state', component: LanguageComponent },
   { path: 'country/:state', component: CountryComponent },
+  { path: 'about/:state', component: ViewComponent },
   { path: '',   redirectTo: '/home/england', pathMatch: 'full' }
 ];
 
